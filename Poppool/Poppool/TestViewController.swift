e75f90b5f5ef63f1f7ffbd4b5726fba79c37b8c0
Madman-dev <dongjunlee222@gmail.com>
2024-11-24 22:07:56 +0900

    ✨:: 스토리보드 제거

---
+++ Poppool/Poppool/TestViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Poppool
+//
+//  Created by Porori on 11/24/24.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
